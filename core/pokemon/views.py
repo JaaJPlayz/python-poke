3f1912fb4f311aab0c745814db8fe1749fc52bb2
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-07-11 16:13:38 -0300

    Limiting the requests, my API is kinda slow ngl

--- core/pokemon/views.py
+++ core/pokemon/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 import requests
-import os
 
 BASE_URL = "https://pokeapi.co/api/v2/"
 
 
 # Create your views here.
 def pokemon_index(request):
-    url = f"{BASE_URL}pokemon/"
+    url = f"{BASE_URL}pokemon?limit=100&offset=0"
     response = requests.get(url)
     data = response.json()
 
@@ -17,7 +16,6 @@ def pokemon_index(request):
         url = pokemon["url"]
         response = requests.get(url)
         data = response.json()
-        print(data)
 
         all_pokemon.append(data)
 
